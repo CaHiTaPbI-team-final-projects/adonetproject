20b9b8f74b211a9c8fc2ef1fdde103f2053ae33f
ZerAtul7 <manekrafter@gmail.com>
2021-05-12 17:24:37 +0300

    feat: added that viewModels: Genres,Groups,Producers,Records

---
+++ DolbitManager/DolbitManager/ViewModels/GroupsViewModels.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using DolbitManager.Models;
+using DolbitManager.EF;
+using DolbitManager.Commands;
+namespace DolbitManager.ViewModels
+{
+    public class GroupsViewModels : INotifyPropertyChanged
+    {
+        private EDDM _EDDM;
+
+        public ObservableCollection<Group> Groups { get; set; }
+
+        public GroupsViewModels()
+        {
+            _EDDM = new EDDM();
+            Groups = new ObservableCollection<Group>();
+            var GroupsList = _EDDM.Groups.ToList();
+            foreach(Group group in GroupsList)
+            {
+                Groups.Add(group);
+            }
+        }
+
+
+
+        private Group _selectedGroup;
+        public Group SelectedGroup
+        {
+            get { return _selectedGroup; }
+            set
+            {
+                _selectedGroup = value;
+
+
+                OnPropertyChanged("SelectedGroup");
+            }
+        }
+
+
+        private RelayCommand _addGroup;
+        public RelayCommand AddGroup
+        {
+            get
+            {
+                return _addGroup ?? (_addGroup = new RelayCommand(obj =>
+                {
+                    Group group = new Group()
+                    {
+                        Id = 0,
+                        Name = "",
+                        //Records = new List<Record>()
+
+                    };
+                    Groups.Add(group);
+                    SelectedGroup = group;
+                }));
+            }
+        }
+
+        private RelayCommand _updateGroup;
+        public RelayCommand UpdateGroup
+        {
+            get
+            {
+                return _updateGroup ?? (_updateGroup = new RelayCommand(obj =>
+                {
+                    var newGroup = Groups.Where(u => u.Id == 0).ToList();
+                    foreach (var gu in newGroup)
+                        _EDDM.Groups.Add(new Group() { Name = gu.Name });
+                    _EDDM.SaveChanges();
+                    System.Windows.MessageBox.Show("New Group added succesfully");
+                }));
+            }
+        }
+
+
+        private RelayCommand _delGroup;
+        public RelayCommand DelGroup
+        {
+            get
+            {
+                return _delGroup ?? (_delGroup = new RelayCommand(obj =>
+                {
+                    var deletedGroup = _EDDM.Groups.Where(c => c.Name == SelectedGroup.Name).FirstOrDefault();
+                    if (deletedGroup != null)
+                    {
+                        _EDDM.Groups.Remove(deletedGroup);
+                        _EDDM.SaveChanges();
+                        System.Windows.MessageBox.Show("Group вырезан succesfully");
+                    }
+                }));
+            }
+        }
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ DolbitManager/DolbitManager/ViewModels/RecordsViewModels.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using DolbitManager.Models;
+using DolbitManager.EF;
+using DolbitManager.Commands;
+namespace DolbitManager.ViewModels
+{
+    public class RecordsViewModels : INotifyPropertyChanged
+    {
+
+
+        private EDDM _EDDM;
+
+        public ObservableCollection<Record> Records { get; set; }
+        public RecordsViewModels()
+        {
+            _EDDM = new EDDM();
+            Records = new ObservableCollection<Record>();
+            var RecordsList = _EDDM.Records.ToList();
+            foreach(Record record in RecordsList)
+            {
+                Records.Add(record);
+            }
+        }
+
+        private Record _selectedRecord;
+        public Record SelectedRecord
+        {
+            get { return _selectedRecord; }
+            set
+            {
+                _selectedRecord = value;
+
+
+                OnPropertyChanged("SelectedRecord");
+            }
+        }
+
+
+        private RelayCommand _addRecord;
+        public RelayCommand AddRecord
+        {
+            get
+            {
+                return _addRecord ?? (_addRecord = new RelayCommand(obj =>
+                {
+                    Record record = new Record()
+                    {
+                        Id = 0,
+                        Name = "",
+                        GroupId = 0,
+                        ProducerId = 0,
+                        TrackCount = 0,
+                        GenreId = 0,
+                        StorageId = 0,
+                        RecordDate = new DateTime(01,09,1939),
+                        BasicPrice = 0,
+                        SalePrice = 0,
+                        //Sales = new List<Sale>()
+                        
+                    };
+                    Records.Add(record);
+                    SelectedRecord = record;
+                }));
+            }
+        }
+
+        private RelayCommand _updateRecord;
+        public RelayCommand UpdateRecord
+        {
+            get
+            {
+                return _updateRecord ?? (_updateRecord = new RelayCommand(obj =>
+                {
+                    var newRecord = Records.Where(u => u.Id == 0).ToList();
+                    foreach (var gu in newRecord)
+                        _EDDM.Records.Add(new Record() { Name = gu.Name });
+                    _EDDM.SaveChanges();
+                    System.Windows.MessageBox.Show("New Record added succesfully");
+                }));
+            }
+        }
+
+
+        private RelayCommand _delRecord;
+        public RelayCommand DelRecord
+        {
+            get
+            {
+                return _delRecord ?? (_delRecord = new RelayCommand(obj =>
+                {
+                    var deletedRecord = _EDDM.Records.Where(c => c.Name == SelectedRecord.Name).FirstOrDefault();
+                    if (deletedRecord != null)
+                    {
+                        _EDDM.Records.Remove(deletedRecord);
+                        _EDDM.SaveChanges();
+                        System.Windows.MessageBox.Show("Record вырезан succesfully");
+                    }
+                }));
+            }
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
